2f71b03abc9555f60a38826ce123783074063eaa
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2018-01-08 00:05:14 -0200

    .

---
+++ linux-backend/setup-apache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+
+virtualhost = """
+NameVirtualHost *:80
+"""
+
+conf_d =  "/etc/httpd/conf.d/a.conf"
+welcome = "/etc/httpd/conf.d/welcome.conf"
+
+  try:
+   os.unlink(welcome)
+  except:
+   pass
+  file = open(conf_d,"w")
+  file.write(virtualhost)
+  file.close()
+
+  os.system("service httpd restart")
+
+
+
