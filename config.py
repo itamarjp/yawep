333054509423bf305537a59357cc4aaba6740abd
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2018-02-13 19:48:51 -0200

    .

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'storage.db')
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/webpanel'
+#SQLALCHEMY_DATABASE_URI = 'postgresql++pg8000://postgres:password@localhost/webpanel'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
 
