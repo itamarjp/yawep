9a47e76a567f5cd44229a1f027de66f10e0a7720
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2018-02-13 21:16:56 -0200

    .

--- linux-backend/install/setup/setup-virtualenv-centos.sh
+++
@@ -1,5 +0,0 @@
-sudo yum -y install python-virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
--- linux-backend/install/setup/setup-virtualenv-fedora.sh
+++
@@ -1,5 +0,0 @@
-sudo dnf -y install python3-virtualenv
-virtualenv-3 venv
-source venv/bin/activate
-pip install -r requirements.txt
-
