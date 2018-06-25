6bd2459285f042e91e630cd885d55a0113cc275d
Itamar Reis Peixoto <itamar@ispbrasil.com.br>
2018-06-24 23:12:54 -0300

    .

--- linux-backend/install/setup/pgsql_post.sh
+++ linux-backend/install/setup/pgsql_post.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 
-/usr/bin/postgresql-setup --initdb --unit postgresql
+sed -i -e 's/local   all             all                                     trust/local   all             all                                     md5/' /var/lib/pgsql/data/pg_hba.conf
+sed -i -e 's/host    all             all             127.0.0.1/32            trust/host   all             all             127.0.0.1/32             md5/' /var/lib/pgsql/data/pg_hba.conf
+sed -i -e 's/host    all             all             ::1/128                 trust/host    all             all             ::1/128                 md5/' /var/lib/pgsql/data/pg_hba.conf
 
-sed -i -e 's/local   all             all                                     peer/local   all             all                                     trust/' /var/lib/pgsql/data/pg_hba.conf
-sed -i -e 's/host    all             all             127.0.0.1/32            ident/host   all             all             127.0.0.1/32             trust/' /var/lib/pgsql/data/pg_hba.conf
-sed -i -e 's/host    all             all             ::1/128                 ident/host    all             all             ::1/128                 trust/' /var/lib/pgsql/data/pg_hba.conf
-
-service postregsql  start
+service postregsql  restart
